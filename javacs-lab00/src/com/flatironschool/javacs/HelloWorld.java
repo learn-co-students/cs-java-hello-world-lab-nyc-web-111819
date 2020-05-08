ad44a763b5e11466b0edb6b80263f95cf96e8d84
Rines.io <tsrines@gmail.com>
2020-05-08 10:36:45 -0400

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,13 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+
+        String version = System.getProperty("java.version");
+        return Double.parseDouble(version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+        getVersion();
+        // you can test the output of getVersion() here
     }
 }
